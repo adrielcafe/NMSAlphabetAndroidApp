3db36e9fc35631b1703ded9924c9385efbfa6eef
Adriel Café <ac@adrielcafe.com>
2016-04-26 11:57:05 -0300

    Parse SDK

--- app/src/main/java/cafe/adriel/nmsalphabet/App.java
+++ app/src/main/java/cafe/adriel/nmsalphabet/App.java
@@ -2,6 +2,12 @@ package cafe.adriel.nmsalphabet;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class App extends Application {
 
@@ -10,6 +16,10 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId(getString(R.string.parse_app_id))
+                .server("http://nmsalphabet.herokuapp.com/api/")
+                .build());
     }
 
     public static void signIn(Context context){
