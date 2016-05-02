4d7785b1961df4730c0310a0afa280850bd9e3f4
Adriel Café <ac@adrielcafe.com>
2016-05-01 22:55:27 -0300

    Footer + getUsersCount()

--- app/src/main/java/cafe/adriel/nmsalphabet/model/AlienWord.java
+++ app/src/main/java/cafe/adriel/nmsalphabet/model/AlienWord.java
@@ -25,6 +25,10 @@ public class AlienWord extends ParseObject implements Serializable {
         put("race", race);
     }
 
+    public String getUsersCount(){
+        return getString("usersCount");
+    }
+
     public ParseRelation getUsers() {
         return getRelation("users");
     }
--- app/src/main/java/cafe/adriel/nmsalphabet/model/User.java
+++ app/src/main/java/cafe/adriel/nmsalphabet/model/User.java
@@ -6,20 +6,20 @@ import java.io.Serializable;
 
 public class User extends ParseUser implements Serializable {
 
-    public String getFacebookUserId() {
-        return getString("facebookUserId");
-    }
-
-    public void setFacebookUserId(String facebookUserId){
-        put("facebookUserId", facebookUserId);
-    }
-
     public String getName(){
         return getString("name");
     }
 
     public void setName(String name){
         put("name", name);
+    }
+
+    public String getFacebookUserId() {
+        return getString("facebookUserId");
+    }
+
+    public void setFacebookUserId(String facebookUserId){
+        put("facebookUserId", facebookUserId);
     }
 
 }
