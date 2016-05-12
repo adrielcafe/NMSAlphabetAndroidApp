028f45ca189e2b449a7c9b72fe31c3cdb7cf9eb3
Adriel Café <ac@adrielcafe.com>
2016-05-11 22:51:57 -0300

    Working on like and dislike feature

--- app/src/main/java/cafe/adriel/nmsalphabet/util/DbUtil.java
+++ app/src/main/java/cafe/adriel/nmsalphabet/util/DbUtil.java
@@ -7,7 +7,9 @@ import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import cafe.adriel.nmsalphabet.App;
 import cafe.adriel.nmsalphabet.model.AlienRace;
 import cafe.adriel.nmsalphabet.model.AlienWord;
 import cafe.adriel.nmsalphabet.model.AlienWordTranslation;
@@ -15,12 +17,15 @@ import cafe.adriel.nmsalphabet.model.User;
 
 public class DbUtil {
     public static int PAGE_SIZE_RACE = 100;
+    public static int PAGE_SIZE_LIKE_DISLIKE = 100;
     public static int PAGE_SIZE_WORD = 50;
     public static int PAGE_SIZE_TRANSLATION = 5;
 
     private static List<AlienRace> races;
+    private static Set<String> likes;
+    private static Set<String> dislikes;
 
-    private static void loadCachedRaces(){
+    private static void loadCachedData(){
         if(races == null){
             try {
                 races = ParseQuery.getQuery(AlienRace.class)
@@ -31,9 +36,35 @@ public class DbUtil {
                 e.printStackTrace();
             }
         }
+        if(likes == null){
+            try {
+                List<AlienWordTranslation> translations = ParseQuery.getQuery(AlienWordTranslation.class)
+                        .whereEqualTo("likes", App.getUser())
+                        .fromLocalDatastore()
+                        .find();
+                for(AlienWordTranslation translation : translations){
+                    likes.add(translation.getObjectId());
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        if(dislikes == null){
+            try {
+                List<AlienWordTranslation> translations = ParseQuery.getQuery(AlienWordTranslation.class)
+                        .whereEqualTo("dislikes", App.getUser())
+                        .fromLocalDatastore()
+                        .find();
+                for(AlienWordTranslation translation : translations){
+                    dislikes.add(translation.getObjectId());
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void cacheRaces(){
+    public static void cacheData(){
         try {
             races = ParseQuery.getQuery(AlienRace.class)
                     .addAscendingOrder("name")
@@ -48,10 +79,29 @@ public class DbUtil {
         } catch (Exception e){
             e.printStackTrace();
         }
+        try {
+            final List<AlienWordTranslation> likes = ParseQuery.getQuery(AlienWordTranslation.class)
+                    .whereEqualTo("likes", App.getUser())
+                    .setLimit(PAGE_SIZE_LIKE_DISLIKE)
+                    .find();
+            final List<AlienWordTranslation> dislikes = ParseQuery.getQuery(AlienWordTranslation.class)
+                    .whereEqualTo("dislikes", App.getUser())
+                    .setLimit(PAGE_SIZE_LIKE_DISLIKE)
+                    .find();
+            AlienWordTranslation.unpinAllInBackground(new DeleteCallback() {
+                @Override
+                public void done(ParseException e) {
+                    AlienWordTranslation.pinAllInBackground(likes);
+                    AlienWordTranslation.pinAllInBackground(dislikes);
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static AlienRace getRaceByName(String name){
-        loadCachedRaces();
+        loadCachedData();
         for(AlienRace race : races){
             if(race.getName().toUpperCase().equals(name.toUpperCase())){
                 return race;
@@ -61,7 +111,7 @@ public class DbUtil {
     }
 
     public static AlienRace getRaceById(String id){
-        loadCachedRaces();
+        loadCachedData();
         for(AlienRace race : races){
             if(race.getObjectId().equals(id)){
                 return race;
@@ -71,7 +121,7 @@ public class DbUtil {
     }
 
     public static int getRacePosition(String id){
-        loadCachedRaces();
+        loadCachedData();
         for(int i = 0; i < races.size(); i++){
             if(races.get(i).getObjectId().equals(id)){
                 return i;
@@ -81,12 +131,38 @@ public class DbUtil {
     }
 
     public static List<String> getRacesName(){
-        loadCachedRaces();
+        loadCachedData();
         List<String> racesName = new ArrayList<>();
         for(AlienRace race : races){
             racesName.add(race.getName());
         }
         return racesName;
+    }
+
+    public static boolean isTranslationLiked(AlienWordTranslation translation){
+        loadCachedData();
+        return likes.contains(translation.getObjectId());
+    }
+
+    public static boolean isTranslationDisliked(AlienWordTranslation translation){
+        loadCachedData();
+        return dislikes.contains(translation.getObjectId());
+    }
+
+    public static void likeTranslation(AlienWordTranslation translation){
+        likes.add(translation.getObjectId());
+        dislikes.remove(translation.getObjectId());
+        translation.addLike(App.getUser());
+        translation.removeDislike(App.getUser());
+        translation.saveEventually();
+    }
+
+    public static void dislikeTranslation(AlienWordTranslation translation){
+        dislikes.add(translation.getObjectId());
+        likes.remove(translation.getObjectId());
+        translation.addDislike(App.getUser());
+        translation.removeLike(App.getUser());
+        translation.saveEventually();
     }
 
     public static void getWords(String word, AlienRace race, int page, FindCallback<AlienWord> callback){
