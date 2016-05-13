a6015bf7125f112e3b566d9c3b33693093bf2043
Adriel Café <ac@adrielcafe.com>
2016-05-12 21:49:14 -0300

    Like and dislike OK

--- app/src/main/java/cafe/adriel/nmsalphabet/ui/adapter/HomeAdapter.java
+++ app/src/main/java/cafe/adriel/nmsalphabet/ui/adapter/HomeAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
