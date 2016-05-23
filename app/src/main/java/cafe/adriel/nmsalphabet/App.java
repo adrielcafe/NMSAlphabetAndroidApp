98dc1d921013c23acde876c9eb45e5da2c97661e
Adriel Café <ac@adrielcafe.com>
2016-05-22 22:28:58 -0300

    Memory leak detection by LeakCanary

--- app/src/main/java/cafe/adriel/nmsalphabet/App.java
+++ app/src/main/java/cafe/adriel/nmsalphabet/App.java
@@ -12,6 +12,7 @@ import com.parse.ParseConfig;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.squareup.leakcanary.LeakCanary;
 import com.tsengvn.typekit.Typekit;
 
 import org.greenrobot.eventbus.EventBus;
@@ -37,7 +38,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
+        LeakCanary.install(this);
         CustomActivityOnCrash.install(this);
         EventBus.getDefault().register(this);
         EasyImage.configuration(this)
